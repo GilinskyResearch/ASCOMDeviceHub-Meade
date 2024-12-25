60e0285566dd31559d02e4d561cb62c911cd9298
David Gilinsky <gilinsky@gilinskyresearch.com>
2024-12-25 14:43:06 -0700

    Add HandboxUpdate message type

---
+++ DeviceHub/Business Object Classes/Message Types/TelescopeHandboxUpdatedMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace ASCOM.DeviceHub
+{
+	public class TelescopeHandboxUpdatedMessage
+    {
+		public TelescopeHandboxUpdatedMessage( TelescopeHandbox handbox)
+		{
+			Handbox = handbox;
+		}
+
+		public TelescopeHandbox Handbox { get; private set; }
+	}
+}
